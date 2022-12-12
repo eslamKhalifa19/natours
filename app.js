f1f5c10d0ac2ddb6aa3e2e455fb28885fe20e837
Eslam Khalefa <eslamkhalefa19@gmail.com>
2022-12-12 20:53:42 +0200

    handling_tours_get_req

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(404).json({ message: 'Hello from the server', app: 'Natours' });
-});
-
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint');
-});
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
