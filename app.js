594789f787f492859732e84596ddbc35b3411792
Eslam Khalefa <eslamkhalefa19@gmail.com>
2022-12-13 00:12:45 +0200

    handling_tour_delete_req

--- app.js
+++ app.js
@@ -58,6 +58,15 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 const port = 3000;
 
 app.listen(port, () => {
