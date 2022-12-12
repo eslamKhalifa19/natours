5ca841dc55462472b8295c73f16de903ee8a7cfb
Eslam Khalefa <eslamkhalefa19@gmail.com>
2022-12-12 23:41:28 +0200

    handling_get_tour_by_id

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
