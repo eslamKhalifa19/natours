b35305bef5720b87e5dc057e29415197801c6c7e
Eslam Khalefa <eslamkhalefa19@gmail.com>
2022-12-13 00:08:18 +0200

    handling_tour_patch_req

--- app.js
+++ app.js
@@ -48,6 +48,16 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour: '<Updated tour here>' },
+  });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
