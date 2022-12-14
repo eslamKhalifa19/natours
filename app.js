3ad273f4fe4fcec10a0b5404a9075dd9e9bdf379
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-14 09:03:21 +0200

    env_port_and_NODE_ENV

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 3000;
-
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
