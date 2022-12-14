091ac034073258627f41128400f87c7fb1891858
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-14 08:26:43 +0200

    chaining_check_body_middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,6 +11,15 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'Missing name or price' });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,12 +6,13 @@ const {
   updateTour,
   deleteTour,
   checkID,
+  checkBody,
 } = require('./../controllers/tourController');
 const router = express.Router();
 
 router.param('id', checkID);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(getAllTours).post(checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
