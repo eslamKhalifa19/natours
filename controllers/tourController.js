fe775b0011cdd931a9c98b7cde61c8b1762006ad
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-14 08:10:46 +0200

    check_ID_param_middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,13 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -16,10 +23,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
-  }
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
@@ -38,7 +41,6 @@ exports.createTour = (req, res) => {
       res.status(201).json({
         status: 'success',
         requestedAt: req.requestTime,
-
         data: {
           tour: newTour,
         },
@@ -48,25 +50,17 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
-  }
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-
     data: { tour: '<Updated tour here>' },
   });
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid Id' });
-  }
   res.status(204).json({
     status: 'success',
     requestedAt: req.requestTime,
-
     data: null,
   });
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,8 +5,11 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  checkID,
 } = require('./../controllers/tourController');
 const router = express.Router();
+
+router.param('id', checkID);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
