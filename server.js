2a8a3f5bee7c4640a8c489014f3cdb8f6ac5cfb7
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-20 03:42:14 +0200

    connect_database_to_express

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection established'));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
