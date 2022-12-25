65131b7295d3452281814dee877d33eef4e4d5e6
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-25 11:59:21 +0200

    add_sorting_in_api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,7 +9,14 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     const tours = await query;
 
