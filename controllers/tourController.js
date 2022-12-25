d2a57cce02cfd7e2c771fcc218bcb35484d9b102
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-25 11:33:10 +0200

    add_filtering_in_api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,17 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
