240c1f9584bd7d0cb9536b48c089606410cfc1df
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-26 10:14:20 +0200

    add_alias_top_tours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,10 +4,13 @@ const {
   createTour,
   getTour,
   updateTour,
-  deleteTour
+  deleteTour,
+  aliasTopTours
 } = require('./../controllers/tourController');
 
 const router = express.Router();
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router
   .route('/')
