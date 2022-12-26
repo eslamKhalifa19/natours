e168da0a99d72bb13cca0c4806c347fcdff1cae3
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-26 04:39:11 +0200

    add_pagination_in_api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,6 +25,16 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This Page is not exist');
+    }
+
     const tours = await query;
 
     res.status(200).json({
