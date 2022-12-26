88d8d8563735cb4e2005aa298c1a0688187b5b1e
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-26 03:48:20 +0200

    add_limiting_fields_in_api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,6 +18,13 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     const tours = await query;
 
     res.status(200).json({
