2d8a83129f3a66c062e9275842a515678637cada
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-27 14:26:03 +0200

    aggregation_middleware_to_hide_secret_tour

--- models/tourModel.js
+++ models/tourModel.js
@@ -83,6 +83,11 @@ tourSchema.pre(/^find/, function(next) {
   next();
 });
 
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
