2a5f5148dcb99f245d596b7a385dacdf92e9d946
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-27 08:43:15 +0200

    aggregation_pipeline_matching_grouping_tours_stats

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,15 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  aliasTopTours
+  aliasTopTours,
+  getTourStats
 } = require('./../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router
   .route('/')
