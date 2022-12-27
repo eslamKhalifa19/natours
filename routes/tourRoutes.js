7bf55a774661aed7f6fbbf7b120f8dc59144e6f0
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-27 09:55:06 +0200

    aggregation_unwinding_projecting_get_monthly_plan

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,7 +6,8 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
-  getTourStats
+  getTourStats,
+  getMonthlyPlan
 } = require('./../controllers/tourController');
 
 const router = express.Router();
@@ -14,6 +15,8 @@ const router = express.Router();
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
+
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router
   .route('/')
