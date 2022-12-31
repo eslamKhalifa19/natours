ccef64b38c5e6fda0702d6615e5b5b3cc7aad1c8
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-31 11:13:55 +0200

    handle_unhandled_routes

--- app.js
+++ app.js
@@ -13,5 +13,10 @@ app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server !`
+  });
+});
 module.exports = app;
