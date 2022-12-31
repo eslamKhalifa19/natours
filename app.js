c82f9b963dca0160edd5e1053a1950315c905c7b
Eslam Khalefa <eslamkhalifa842@gmail.com>
2022-12-31 12:02:21 +0200

    add_global_error_handling_middleware

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -14,9 +17,8 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server !`
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+app.use(globalErrorHandler);
 module.exports = app;
