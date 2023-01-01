bca55fc44f18eb28ba27a5b3d70349ef24e53c39
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-01 16:12:40 +0200

    catch_uncaught_exceptions

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION ! Shutting down ...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -26,8 +32,9 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('Unhandled rejection ! Shutting down ...');
+  console.log(err.name, err.message);
+
   server.close(() => {
     process.exit(1);
   });
