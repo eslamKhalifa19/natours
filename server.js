b6f3300003dabf3084fb4417b9a0340dc3c3b5e5
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-01 15:44:40 +0200

    errors_outside_express_unhandled_rejections

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ mongoose
   .then(() => console.log('DB connection established'));
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`listening on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled rejection ! Shutting down ...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
