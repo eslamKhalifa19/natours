0b01e664f2d48b1122643a63aa82e773c41523bf
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-02 13:57:03 +0200

    logging_user_using_jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,14 @@ const User = require('./../models/userModel');
 
 const catchAsync = require('./../utils/catchAsync');
 
+const AppError = require('./../utils/appError');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -12,14 +20,28 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
-  });
+  const token = signToken(newUser._id);
   res.status(201).json({
     status: 'success',
     token,
     data: {
       user: newUser
     }
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token
   });
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,37 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined ',
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
   });
-};
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined ',
+    message: 'This route is not yet defined '
   });
 };
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined ',
+    message: 'This route is not yet defined '
   });
 };
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined ',
+    message: 'This route is not yet defined '
   });
 };
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined  ',
+    message: 'This route is not yet defined  '
   });
 };
