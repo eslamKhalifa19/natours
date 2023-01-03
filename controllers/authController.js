931b01c798488dbeff2a85bb639ec554ed75e441
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-03 17:24:54 +0200

    authorization_user_roles_and_permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -82,3 +82,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
+};
