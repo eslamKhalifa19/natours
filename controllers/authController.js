0896a752c30344d0f29b6112c0c22806986f9895
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-03 08:43:13 +0200

    protect_route__getting_token_and_check_it

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,3 +45,19 @@ exports.login = catchAsync(async (req, res, next) => {
     token
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in please log in to get access')
+    );
+  }
+  next();
+});
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,7 @@ const {
   getTourStats,
   getMonthlyPlan
 } = require('./../controllers/tourController');
+const { protect } = require('./../controllers/authController');
 
 const router = express.Router();
 
@@ -20,7 +21,7 @@ router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router
   .route('/')
-  .get(getAllTours)
+  .get(protect, getAllTours)
   .post(createTour);
 router
   .route('/:id')
