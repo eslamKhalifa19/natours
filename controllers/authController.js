281b129854946921e7da000859086a5362d5bd91
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-08 12:46:50 +0200

    setting_new_password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const { promisify } = require('util');
 
 const jwt = require('jsonwebtoken');
@@ -135,4 +137,29 @@ exports.forgetPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: new Date() }
+  });
+
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token
+  });
+});
