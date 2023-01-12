f88491beaa2c2a63ef725d0f25310867ffbac56c
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-12 11:27:43 +0200

    implement_rate_limiting

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
-const AppError = require('./utils/appError');
+const rateLimit = require('express-rate-limit');
 
+const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -11,6 +12,14 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP , please try again in an hour'
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 
