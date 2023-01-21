4262a2785ea8cdb7ca24d21aa97b37db45461616
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-21 17:42:39 +0200

    modelling_tour_guides_child_referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -97,6 +97,12 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
     ]
   },
   {
