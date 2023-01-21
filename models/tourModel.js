a6086c0c984658e5852e851d39653a2b4b99135e
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-21 19:46:35 +0200

    virtual_populate_tours_and_reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -115,6 +115,11 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
+});
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
