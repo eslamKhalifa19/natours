a7ccd926f372eb6e374c648017006d7a677ecd46
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-21 19:26:55 +0200

    creating_and_getting_reviews

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {
+  getAllReviews,
+  createReview
+} = require('./../controllers/reviewController');
+const { protect, restrictTo } = require('./../controllers/authController');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(getAllReviews)
+  .post(protect, restrictTo('user'), createReview);
+
+module.exports = router;
