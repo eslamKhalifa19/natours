7b7c135179518e1d1b43f5db917cdd5be84991a8
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-02-07 19:37:42 +0200

    building_tour_page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const { getOverview, getTour } = require('../controllers/viewController');
 const router = express.Router();
 
 router.get('/', getOverview);
-router.get('/tour', getTour);
+router.get('/tour/:slug', getTour);
 
 module.exports = router;
