3885085512020327929c61d79f91faa1f1b61a6c
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-02-07 17:27:08 +0200

    building_the_tour_overview

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,14 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
