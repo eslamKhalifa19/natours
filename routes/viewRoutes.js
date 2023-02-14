83b71566bb65d3c9d586d2bf55c9b3c129f3dd80
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-02-14 05:04:35 +0200

    building_login_screen

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { getOverview, getTour } = require('../controllers/viewController');
+const {
+  getOverview,
+  getTour,
+  getLoginForm
+} = require('../controllers/viewController');
 
 const router = express.Router();
 
 router.get('/', getOverview);
 router.get('/tour/:slug', getTour);
+router.get('/login', getLoginForm);
 
 module.exports = router;
